406aebc9047c9ee7e38465d0f3c143644912e3d8
shehzadkubra <33358971+shehzadkubra@users.noreply.github.com>
2017-11-08 11:28:43 -0500

    models for bills

---
+++ iAssistAPI/iAssistAPI/Models/Bills.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace iAssistAPI.Models
+{
+    /// <summary>
+    /// Bills model
+    /// </summary>
+    public class Bills
+    {
+        public string AcctNo { get; set; }
+
+        public string InvoiceNo { get; set; }
+
+        public string DueDate { get; set; }
+
+        public Decimal Amt { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string Status { get; set; }
+    }
+}
